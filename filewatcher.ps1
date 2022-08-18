4957312410d4ef5fc9769197516b433fb0226518
Jae Choi <jaehyoung.choi@mail.utoronto.ca>
2022-08-18 09:57:19 -0400

    simplefix

--- filewatcher.ps1
+++ filewatcher.ps1
@@ -9,18 +9,21 @@ $Watcher.EnableRaisingEvents = $true
 ## Defining Paths to Rscript and the script for data processing
 $RPath = "C:\Program Files\R\R-4.1.3\bin\Rscript.exe"
 $RScript = "C:\PathtoRscript"
-$Destination = "C:\Destination\output"
+## Defining Destination for R output file - Rscript should receive '/' operated filedir instead of naive MSOS "\" dirsys - this is also repeated in the Action definition for input to R
+$Destination = "C:/Destination/output"
 
 ## Defining actions to be made when file creation is detected
 $action = {
 	$filepath = $Event.SourceEventArgs.FullPath
 	$filename = $Event.SourceEventArgs.Name
 
-	$Rpath $Rscript $filepath $filename $Destination
+	$filepathR = $filepath -replace '\\', '/'
+
+	$Rpath $Rscript $filepathR $filename $Destination
 }
 
 ## Detecting Created Files and Executing Actions.
-Register-ObjectEvent $Watcher "Created" -Action $action
+Register-ObjectEvent -InputObject $Watcher Created -Action $action
 
 while ($true) {sleep 10}
-
+## or while ($true) {Unregister-Event -SourceIdentifier 'Created'}
